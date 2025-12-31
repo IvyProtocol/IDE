dda9222a56b43c19cba3077a29639342a38999ec
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2025-12-31 20:29:14 +0600

    Update globalvariable.sh

--- Configs/.local/bin/globalvariable.sh
+++ Configs/.local/bin/globalvariable.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-export confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
-export localDir="${XDG_LOCAL_HOME:-$HOME/.local}"
-export cacheDir="${XDG_CACHE_HOME:-$HOME/.cache}"
+export homDir="${XDG_HOME:-$HOME}"
+export confDir="${XDG_CONFIG_HOME:-${homDir}/.config}"
+export localDir="${XDG_LOCAL_HOME:-${homDir}/.local}"
+export cacheDir="${XDG_CACHE_HOME:-${homDir}/.cache}"
 export swayncDir="${XDG_SWAYNC_ICON:-${confDir}/swaync}"
 export rofiStyleDir="${XDG_RSDIR_HOME:-${confDir}/rofi}/styles"
 export rofiAssetDir="${XDG_RADIR_HOME:-${confDir}/rofi/shared}/assets"
