fd6eca120a3fab36bf7c426da1de2feb7b1dec12
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-01-16 22:39:56 +0600

    Refactor wallpaper selection script with new options

--- Configs/.local/bin/wbsecrandom.sh
+++ Configs/.local/bin/wbsecrandom.sh
@@ -1,7 +1,66 @@
 #!/usr/bin/env bash
+set -euo pipefail
 scrDir=$(dirname "$(realpath "$0")")
 source "$scrDir/globalvariable.sh"
 
-wallpapers_dir="$HOME/Pictures/wallpapers"
-random_wallpaper=$(find "$wallpapers_dir" -maxdepth 1 -type f | shuf -n 1)
-$localDir/bin/wbselecgen.sh "$random_wallpaper"
+pxCheck="${1:-}"
+walCheck="${2:-}"
+wallDir="${homDir}/Pictures/wallpapers"
+
+swww-prefix() {
+    local walRasi="${cacheDir}/ivy-shell/blurred/current_wallpaper.rasi"
+    local wpex filterPath extract_wall wall_i candidate_i pxCheck
+   
+    pxCheck="${1:-}"
+    wpex=$(grep -oE '"/[^"]+"' "$walRasi") || return 1
+    fillPath="${wpex#\"}"
+    fillPath="${fillPath%\"}"
+    extract_wall="${fillPath##*/}"
+    wall="${extract_wall}"
+
+    [[ -n "${wall}" ]] || return 1
+
+    mapfile -t wallpapers < <( LC_ALL=C find "$wallDir" -maxdepth 1 -type f -printf '%f\n' | sort -V )
+
+    wall_i=-1
+    for i in "${!wallpapers[@]}"; do
+        [[ "${wallpapers[$i]}" == "${wall}" ]] && wall_i=$i
+    done
+
+    total=${#wallpapers[@]}
+    local dirFlag
+    case "$pxCheck" in
+        --p) idx=$(( (wall_i - 1 + total) % total )); dirFlag=0 ;;
+        --n) idx=$(( (wall_i + 1) % total )); dirFlag=1 ;;
+        --r) render ;; 
+        *) return 1 ;;
+    esac
+    rand="$wallDir/${wallpapers[$idx]}"
+    if [[ "$dirFlag" -eq 0 ]]; then
+        ${scrDir}/wbselecgen.sh "${rand}" --swww-p
+        echo "* { current-image: url(\"$rand\", height); }" > "${walRasi}" &
+    elif [[ "$dirFlag" -eq 1 ]]; then
+        ${scrDir}/wbselecgen.sh "${rand}" --swww-n
+        echo "* { current-image: url(\"$rand\", height); }" > "${walRasi}" &
+    fi
+}
+
+render() {
+    rmDir=$(find "$wallDir" -maxdepth 1 -type f | shuf -n 1)
+    ${scrDir}/wbselecgen.sh "$rmDir"
+}
+
+case "$pxCheck" in
+    -p|--previous) 
+        swww-prefix --p 
+        ;;
+    -n|--next)
+        swww-prefix --n
+        ;;
+    -r|--random)
+        render
+        ;;
+    *)
+        echo -e "Invalid '$pxCheck' for argument. Correct arguments are: -p (--previous), -n (--next), -r (--random)"
+        exit 0
+esac
