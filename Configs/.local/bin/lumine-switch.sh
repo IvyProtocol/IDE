a316b07b01e53d5f2aa26f223eb1b23ae56c79a3
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-01-20 16:39:00 +0600

    Update lumine-switch.sh

--- Configs/.local/bin/lumine-switch.sh
+++ Configs/.local/bin/lumine-switch.sh
@@ -10,17 +10,31 @@ rasiPath="${rasiDir}/shell.rasi"
 apply_config() {
     local rasiTarget="${cacheDir}/ivy-shell"
     local awCheck=$(awk 'NR == 2' "${rasiTarget}/cache.rasi")
+    local wallDir="${homDir}/Pictures/wallpapers"
+
     if [[ -z "$awCheck" ]]; then
         echo "Cshell=\"$1\"" >> "${rasiTarget}/cache.rasi"
     else
         sed -i "s|^Cshell=.*|Cshell=\"$1\"|" "${rasiTarget}/cache.rasi"
     fi
 
-    [[ -e "${scrDir}/wbselecgen.sh" ]] && "${scrDir}/wbselecgen.sh" -s
+    [[ ! -e "${scrDir}/ivy-shell.sh" ]] && exit 1
+    ext="${wallDir}/$(fl_wallpaper)"
+    if [[ "$1" == "dark" ]]; then
+        "${scrDir}/ivy-shell.sh" "$ext" -d
+    elif [[ "$1" == "light" ]]; then
+        "${scrDir}/ivy-shell.sh" "$ext" -l
+    elif [[ "$1" == "auto" ]]; then
+        "${scrDir}/ivy-shell.sh" "$ext" -a
+    fi
+    if [[ -z "${ext}" && -x "${scrDir}/wbselecgen.sh" ]]; then
+        rnSel=$(find "${wallDir}" -maxpath 1 -type f | shuf -n 1) \
+        "${scrDir}/wbselecgen.sh" -i "${rnSel}"
+    fi
 }
 
 main() {
-    options=(dark light auto) 
+    options=(auto dark light) 
 
     choice=$(printf '%s\n' "${options[@]}" \
         | rofi -i -dmenu \
@@ -31,7 +45,6 @@ main() {
     [[ -z "$choice" ]] && { echo "No option selected. Exiting."; exit 0; }
     
     apply_config "$choice"
-    
 }
 
 if pgrep -x "rofi" >/dev/null; then
