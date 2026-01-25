80e77d0cf762fa0103616c1389558aa44d70d8d0
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-01-25 18:17:37 +0600

    Update qtvantum.sh

--- Configs/.local/bin/qtvantum.sh
+++ Configs/.local/bin/qtvantum.sh
@@ -11,14 +11,14 @@ if pgrep -f "polkit-gnome-authentication-agent-1" > /dev/null; then
     # Kill existing agent properly
     pkill -f "polkit-gnome-authentication-agent-1"
 fi
-    hyprctl dispatch exec "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" >/dev/null 2>&1 &
+    /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
 
 if pgrep -f "xdg-desktop-portal-gtk" > /dev/null; then
     pkill -f "xdg-desktop-portal-gtk"
 fi
-    hyprctl dispatch exec "/usr/lib/xdg-desktop-portal-gtk" >/dev/null 2>&1 &
+    /usr/lib/xdg-desktop-portal-gtk >/dev/null 2>&1 &
 
 if pgrep -f "xdg-desktop-portal-hyprland" > /dev/null; then
     pkill -f "xdg-desktop-portal-hyprland"
 fi
-    hyprctl dispatch exec "/usr/lib/xdg-desktop-portal-hyprland" >/dev/null 2>&1 &
+    /usr/lib/xdg-desktop-portal-hyprland >/dev/null 2>&1 &
