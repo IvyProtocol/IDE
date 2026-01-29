9dbfa32b4e8379971bb1e039b14d420e80fb6b62
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-01-30 00:34:02 +0600

    Update config.fish

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -1,8 +1,10 @@
 if status is-interactive
-	#set -U fish_greeting ""
 	starship init fish | source
 
 	alias pamcan pacman
 	alias fastfetch="$HOME/.local/bin/fastfetch.sh"
+	alias env_pkg="$HOME/.config/fish/fish_scripts/env_pkg.sh"
+	alias wbselecgen="$HOME/.local/bin/wbselecgen.sh"
+	alias zimg="kitty +kitten icat"
 	set EDITOR nvim
 end
