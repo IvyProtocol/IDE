48f6245d13d8387306648bb02d77f15e66b53ab3
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-01-30 00:36:52 +0600

    Update env_pkg.sh

--- Configs/.config/fish/fish_scripts/env_pkg.sh
+++ Configs/.config/fish/fish_scripts/env_pkg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-[[ -f "$HOME/.local/bin/globalcontrol.sh" ]] && source "$HOME/.local/bin/globalcontrol.sh" || echo "$HOME/.local/bin/globalcontrol.sh does not exist!" && exit 1
+[[ ! -f "$HOME/.local/bin/globalcontrol.sh" ]] && echo "[$0] $HOME/.local/bin/globalcontrol.sh" && exit 1 || source "$HOME/.local/bin/globalcontrol.sh"
 
 srcf_rcall env_pkg && [[ $? -ge 1 ]] && echo "[$0]: function {env_pkg} does NOT exist!" && exit 1
 env_pkg "$@"
