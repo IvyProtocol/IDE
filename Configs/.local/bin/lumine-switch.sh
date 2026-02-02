d0ad0a0d5510746c54a701f71332724e106542f1
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-02-02 16:29:36 +0600

    Update lumine-switch.sh

--- Configs/.local/bin/lumine-switch.sh
+++ Configs/.local/bin/lumine-switch.sh
@@ -6,24 +6,18 @@ source "$scrDir/globalcontrol.sh"
 rasiPath="${rasiDir}/shell.rasi"
 
 apply_config() {
-    local ideDir="${confDir}/ivy-shell"
 
     [[ $1 == "auto" ]] && wallIde=0
     [[ $1 == "dark" ]] && wallIde=1
     [[ $1 == "light" ]] && wallIde=2
 
-    [[ -n "${enableWallIde}" ]] && sed -i "s|^enableWallIde=[0-9]|enableWallIde=${wallIde}|" "${ideDir}/ide.conf" || sed -i "s|^enableWallIde=.*|enableWallIde=0|" "${ideDir}/ide.conf"
+    [[ -n "${enableWallIde}" ]] && setConf "enableWallIde" "${wallIde}" "${ideDir}/ide.conf" || setConf "enableWallIde" "0" "${ideDir}/ide.conf"
     notify -m 1 -p "Theme Mode: $1" -s "${swayncDir}/icons/palette.png"
 
     [[ ! -e "${scrDir}/ivy-shell.sh" ]] && exit 1
-    ext="${wallSet}"
-    [[ "${wallIde}" -eq 0 ]] && "${scrDir}/ivy-shell.sh" -i "$ext" -c auto  
-    [[ "${wallIde}" -eq 1 ]] && "${scrDir}/ivy-shell.sh" -i "$ext" -c dark 
-    [[ "${wallIde}" -eq 2 ]] && "${scrDir}/ivy-shell.sh" -i "$ext" -c light
+    [[ -n "${wallIde}" ]] && "${scrDir}/ivy-shell.sh" -i "${wallSet}" -c "${1}" 
 
-
-
-    if [[ -z "${ext}" && -x "${scrDir}/wbselecgen.sh" ]]; then
+    if [[ -z "${wallSet}" && -x "${scrDir}/wbselecgen.sh" ]]; then
         rnSel=$(find "${wallDir}" -maxpath 1 -type f | shuf -n 1)
         "${scrDir}/wbselecgen.sh" -i "${rnSel}"
     fi
