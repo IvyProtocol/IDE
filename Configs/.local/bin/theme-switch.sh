61bd815c656d5de38bb3ee659ee26e488645adf4
Ivy <121424993+IvyProtocol@users.noreply.github.com>
2026-02-05 17:58:02 +0600

    Update theme-switch.sh

--- Configs/.local/bin/theme-switch.sh
+++ Configs/.local/bin/theme-switch.sh
@@ -12,30 +12,32 @@ rofiConf="${rasiDir}/selector.rasi"
 log() { echo "[$0] "$@""; }
 
 # ────────────────────────────────────────────────
-# Apply wallpaper + blur + cache + color sync
-apply_wallpaper() {
+themeSelTui() {
     thmChsh="${1}"
-    [[ "${themeIde}" == "${1}" ]] || setConf "themeIde" "${1}" "${scrDir}/globalcontrol.sh" 
+    [[ "${PrevThemeIde}" == "${thmChsh}" ]] || setConf "PrevThemeIde" "${thmChsh}" "${scrDir}/globalcontrol.sh" 
     [[ "${wallDir}" == "${ideDir}/theme/${1}/wallpapers" ]] || setConf "wallDir" "\${XDG_CONFIG_HOME:-\$HOME/.config}/ivy-shell/theme/${1}/wallpapers" "${ideDir}/ide.conf"
-
+    
     if [[ "${enableWallIde}" -eq 3 ]]; then
-        [[ "${ideTheme}" == "$1" ]] || setConf "ideTheme" "$1" "${ideDir}/ide.conf"
+        [[ "${ideTheme}" == "${thmChsh}" ]] || setConf "ideTheme" "$1" "${ideDir}/ide.conf"
         sed -Ei 's|^[[:space:]]*source[[:space:]]*=[[:space:]]*./themes/wallbash-ide.conf|#source = ./themes/wallbash-ide.conf|' "${confDir}/hypr/hyprland.conf"
     else
         sed -Ei 's|^#[[:space:]]*source[[:space:]]*=[[:space:]]*./themes/wallbash-ide.conf|source = ./themes/wallbash-ide.conf|' "${confDir}/hypr/hyprland.conf"
     fi
 
-    if [[ ! -f "${ideDir}/theme/${themeIde}/wallpapers/.wallbash.main" ]]; then
-        echo "${ideDir}/theme/${themeIde}/wallpapers/1_rain_world.png" > "${ideDir}/theme/${themeIde}/wallpapers/.wallbash.main" &
+    unset PrevThemeIde
+    PrevThemeIde="${thmChsh}"
+    PreProcess="${ideDir}/theme/${PrevThemeIde}/wallpapers"
+    echo "2"
+    if [[ ! -f "${PreProcess}/.wallbash.main" ]]; then
+        echo "$(find "${PreProcess}" -mindepth 1 -maxdepth 1 -type f ! -name ".wallbash.main" | shuf -n 1 )" > "${PreProcess}/.wallbash.main" 
     fi
-    img="$(cat "${ideDir}/theme/${1}/wallpapers/.wallbash.main")"
 
+    img="$(cat "${ideDir}/theme/${PrevThemeIde}/wallpapers/.wallbash.main")"
     [[ ! -e "${scrDir}/wbselecgen.sh" ]] && notify -m 1 -p "Does wbselecgen.sh exist?" -s "${swayncDir}/icons/palette.png" && return 1 || "${scrDir}/wbselecgen.sh" -i "${img}" -w --swww-t -n
 }
 
 # ────────────────────────────────────────────────
-# Rofi Settings
-expV() {
+rSettings() {
     [[ -z "${rofiScale}" ]] && rofiScale=10 || rofiScale="${rofiScale}"
     r_scale="configuration {font : \"JetBrainsMono Nerd Font ${rofiScale}\";}"
     mon_x_res=$(( mon_res * 100 / mon_scale ))
@@ -49,8 +51,7 @@ expV() {
 }
 
 # ────────────────────────────────────────────────
-# Interactive wallpaper picker
-choose_wallpaper() {
+thmSelEnv() {
     mapfile -t themes < <(LC_ALL=C find "${themeDir}" -mindepth 1 -maxdepth 1 -type d -printf '%f\n' | sort -Vf)
 
     menu() {
@@ -61,16 +62,10 @@ choose_wallpaper() {
             printf "%s\x00icon\x1f%s\n" "${indx}" "${wallSet}"
         done
     }
-    expV
+    rSettings
     choice=$(menu | rofi -dmenu -i -p "ThemeControl" -theme-str "${r_scale}" -theme-str "${r_override}" -config "${rofiConf}" -select "${selectC}")
     [[ -z "$choice" ]] && exit 0
-    apply_wallpaper "$choice"
+    themeSelTui "$choice"
 }
 
-# ────────────────────────────────────────────────
-# Main
-if [ -n "$1" ]; then
-    apply_wallpaper "$@"
-else
-    choose_wallpaper
-fi
+[[ -z "${1}" ]] && thmSelEnv || themeSelTui "$1"
